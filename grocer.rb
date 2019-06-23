3d018e2501e9b669732880c79943f47416127445
Ursula Miller <ursula.miller@gmail.com>
2019-06-23 23:46:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,18 +35,18 @@ def apply_coupons(cart, coupons)
      sum_cart["#{coupon[:item]} W/COUPON"][:clearance] = true
  #    binding.pry
    end
-    cart.each do |item, attribute|
-        coupons.each do |coupon|
-          if coupon[:item] == item
-            sum_cart[item][:count] = sum_cart[item][:count] - coupon[:num]
-          else
-            sum_cart[item] = {}
-            sum_cart[item] = attribute
-#            binding.pry
-          end
+  cart.each do |item, attribute|
+    coupons.each do |coupon|
+      if coupon[:item] == item
+        sum_cart[item][:count] = sum_cart[item][:count] - coupon[:num]
+        else
+          sum_cart[item] = {}
+          sum_cart[item] = attribute
+#          binding.pry
         end
+      end
     end
-
+  end
 #  binding.pry
   sum_cart
 end
