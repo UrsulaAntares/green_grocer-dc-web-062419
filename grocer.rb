d0fa5ab84d78491303fd45f3e28bf26b894b94bb
Ursula Miller <ursula.miller@gmail.com>
2019-06-23 22:21:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,61 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  tally = {}
+  cart.each do |line_item|
+    line_item.each do |item, attribute|
+      tally[item] = {} 
+      tally[item][:count] = 0
+      attribute.each do |detail, spec| tally[item][detail] = spec
+      end
+    end
+  end
+  cart.each do |line_item|
+    line_item.each do |item, attribute|
+      tally[item][:count] += 1
+    end
+  end  
+    tally
 end
 
+
+###############################################
+
+
+
 def apply_coupons(cart, coupons)
-  # code here
+  sum_cart = cart
+  cart.each do |line_item|
+    line_item.each do |item, attribute|
+      coupons.each do | coupon |
+        binding.pry
+        if coupon[:item] == item
+          sum_cart[item][:count] = sum_cart[item][:count] - coupon[:item][:count]
+        end
+      end
+    end
+  end
 end
+
+
+
+
 
 def apply_clearance(cart)
   # code here
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidate_cart(cart)
 end
+
+
+cart =[
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
+
+coupons = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
+#checkout(cart, coupons)
