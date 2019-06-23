f6e05a901ccca089f722ebd0c85b4b565708ea15
Ursula Miller <ursula.miller@gmail.com>
2019-06-23 23:30:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,6 @@ def apply_coupons(cart, coupons)
      #     binding.pry
           if coupon[:item] == item
             sum_cart[item][:count] = sum_cart[item][:count] - coupon[:num]
-            binding.pry
           else
             sum_cart[item] = {}
             binding.pry
