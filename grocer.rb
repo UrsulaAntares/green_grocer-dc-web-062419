3843453d6a31906168be54e580050b9d25524d12
Ursula Miller <ursula.miller@gmail.com>
2019-06-24 00:06:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,6 +39,7 @@ def apply_coupons(cart, coupons)
     coupons.each do |coupon|
       if coupon[:item] == item
         sum_cart[item][:count] = sum_cart[item][:count] - coupon[:num]
+        binding.pry
       else
         sum_cart[item] = {}
         sum_cart[item] = attribute
