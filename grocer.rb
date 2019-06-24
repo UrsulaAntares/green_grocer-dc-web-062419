fecaca249cb892b2f5ac56e03470efabcadfe9fe
Ursula Miller <ursula.miller@gmail.com>
2019-06-24 01:11:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,6 +40,7 @@ def apply_coupons(cart, coupons)
               sum_cart["#{coupon[:item]} W/COUPON"] = {}   
             end
             coupons.each do |coupon|
+              binding.pry
               sum_cart["#{coupon[:item]} W/COUPON"][:count] = coupon[:num]
               sum_cart["#{coupon[:item]} W/COUPON"][:price] = (coupon[:cost] / coupon[:num])
             end
